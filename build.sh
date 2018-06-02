1482d62febd21746aa734a528193fc4f96b1ec86
Auston Stewart <auston@shedbuilt.net>
2018-06-01 21:59:38 -1000

    Removing bad man page manipulation from build script

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Build and Install
 python3 setup.py build &&
-python3 setup.py install --root "$SHED_FAKE_ROOT" &&
-# Rearrange man pages
-mv -v "${SHED_FAKE_ROOT}/usr/lib/python3.6/site-packages/${SHED_PKG_NAME}-${SHED_PKG_VERSION}-py3.6.egg/share/man/man1"/*.1 \
-      "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
-rm -r "${SHED_FAKE_ROOT}/usr/lib/python3.6/site-packages/${SHED_PKG_NAME}-${SHED_PKG_VERSION}-py3.6.egg/share"
+python3 setup.py install --root "$SHED_FAKE_ROOT"
